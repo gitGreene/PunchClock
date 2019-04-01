f81ae4d8fabfe7aa596cf53430cd86e5bb6db0fb
Mitch Greene <m.iangreene@gmail.com>
2019-04-01 18:24:46 -0400

    Goal, Category Dao Test Changes

--- app/src/androidTest/java/codemaestro/co/punchclock/CategoryDaoOperationsTest.java
+++ app/src/androidTest/java/codemaestro/co/punchclock/CategoryDaoOperationsTest.java
@@ -31,15 +31,30 @@ public class CategoryDaoOperationsTest {
     private Database database;
     private CategoryDao categoryDao;
 
+    private static final int CATEGORY_1_ID = 1;
+    private static final String CATEGORY_1_NAME = "Category 1";
+    private static final String CATEGORY_1_DESC = "Category 1 Description";
+    private static final long CATEGORY_1_TOTALTIME = 0L;
+    private static final String CATEGORY_1_DATECREATED = "Category 1 Date Created";
+    private static final boolean CATEGORY_1_ISFAVORITE = false;
+
+
+    private static final int CATEGORY_2_ID = 2;
+    private static final String CATEGORY_2_NAME = "Category 2";
+    private static final String CATEGORY_2_DESC = "Category 2 Description";
+    private static final long CATEGORY_2_TOTALTIME = 1000L;
+    private static final String CATEGORY_2_DATECREATED = "Category 2 Date Created";
+    private static final boolean CATEGORY_2_ISFAVORITE = true;
+
     @Before
     public void initDb() throws Exception {
         database = Room.inMemoryDatabaseBuilder(InstrumentationRegistry.getContext(), Database.class).allowMainThreadQueries().build();
 
         categoryDao = database.categoryDao();
-        Category testCategory = new Category("Test Category", "Test Description", 0L, "Test Date Created", false);
+        Category testCategory = new Category(CATEGORY_1_NAME, CATEGORY_1_DESC, CATEGORY_1_TOTALTIME, CATEGORY_1_DATECREATED, CATEGORY_1_ISFAVORITE);
         categoryDao.insertCategory(testCategory);
-        Category testFavoriteCategory = new Category("Test Favorite Category", "Favorite Category Description", 500L, "Favorite Date Created", true);
-        categoryDao.insertCategory(testFavoriteCategory);
+        Category favoriteTestCategory = new Category(CATEGORY_2_NAME, CATEGORY_2_DESC, CATEGORY_2_TOTALTIME, CATEGORY_2_DATECREATED, CATEGORY_2_ISFAVORITE);
+        categoryDao.insertCategory(favoriteTestCategory);
     }
 
     @After
@@ -50,26 +65,25 @@ public class CategoryDaoOperationsTest {
     @Test
     public void confirmCategoryInsertion() throws InterruptedException {
         List<Category> allCategories = LiveDataTestUtil.getValue(categoryDao.getAllCategories());
-        assertEquals("Test Category", allCategories.get(0).getCategoryName());
+        assertEquals(CATEGORY_1_NAME, allCategories.get(0).getCategoryName());
+    }
+
+    @Test
+    public void getAllCategories_SizeExpected_2() throws InterruptedException {
+        List<Category> allCategories = LiveDataTestUtil.getValue(categoryDao.getAllCategories());
+        assertEquals(2, allCategories.size());
     }
 
     @Test
     public void getCategoryByName() throws InterruptedException {
-        Category testCategory = LiveDataTestUtil.getValue(categoryDao.getCategoryByName("Test Category"));
-        assertEquals("Test Category", testCategory.getCategoryName());
+        Category testCategory = LiveDataTestUtil.getValue(categoryDao.getCategoryByName(CATEGORY_1_NAME));
+        assertEquals(CATEGORY_1_NAME, testCategory.getCategoryName());
     }
 
     @Test
-    public void confirmCategoryIsNotFavorite() throws InterruptedException {
-        List<Category> allCategories = LiveDataTestUtil.getValue(categoryDao.getAllCategories());
-        assertEquals("Test Category", allCategories.get(0).getCategoryName());
-    }
-
-    @Test
-    public void confirmCategoryIsFavorite() throws InterruptedException {
+    public void getAllFavoriteCategories_SizeExpected_1() throws InterruptedException {
         List<Category> favoriteCategories = LiveDataTestUtil.getValue(categoryDao.getFavorites());
-        assertEquals("Test Favorite Category", favoriteCategories.get(0).getCategoryName());
+        assertEquals(1, favoriteCategories.size());
     }
-
 
 }
