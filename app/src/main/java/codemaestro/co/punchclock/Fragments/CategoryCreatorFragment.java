e2ef2ffe486e3b3a0ca79f34ec888c62fd16bfb9
Mitch Greene <m.iangreene@gmail.com>
2019-05-22 23:02:45 -0400

    Cut out that which does not work or matter

--- app/src/main/java/codemaestro/co/punchclock/Fragments/CategoryCreatorFragment.java
+++ app/src/main/java/codemaestro/co/punchclock/Fragments/CategoryCreatorFragment.java
@@ -10,61 +10,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import codemaestro.co.punchclock.Adapters.CategoryWizardAdapter;
 import codemaestro.co.punchclock.R;
 import codemaestro.co.punchclock.ViewModel.CreateCategoryViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CategoryCreatorFragment extends Fragment implements CreateHealthCategoryFragment.ContinueButtonClicked {
-    private int templateId;
-    private CategoryWizardAdapter adapter;
-    private ViewPager viewPager;
-    int x;
-
+public class CategoryCreatorFragment extends Fragment {
 
     public CategoryCreatorFragment() {
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_category_creator, container, false);
-        CreateCategoryViewModel viewModel = ViewModelProviders.of(this).get(CreateCategoryViewModel.class);
 
-
-
-        int templateId = getArguments().getInt("TEMPLATE_ID");
-
-        viewPager = view.findViewById(R.id.templateQuestionsViewPager);
-        adapter = new CategoryWizardAdapter(getChildFragmentManager(), templateId, this);
-        viewPager.setAdapter(adapter);
-        viewPager.setCurrentItem(1, true);
-
-
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int i, float v, int i1) {
-                Toast.makeText(getContext(), "onPageScrolled", Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onPageSelected(int i) {
-                Toast.makeText(getContext(), "onPageSelected", Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int i) {
-                Toast.makeText(getContext(), "onPageScrollStateChanged", Toast.LENGTH_LONG).show();
-            }
-        });
         return view;
     }
 
-    @Override
-    public void continueButtonClicked() {
-        viewPager.setCurrentItem(viewPager.getCurrentItem()+1);
-    }
 }
 
