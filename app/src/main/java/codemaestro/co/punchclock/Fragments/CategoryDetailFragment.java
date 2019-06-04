598605d3cc6a978c3b3a79fb97e6b16bcae0571b
Francis <francisglacken@gmail.com>
2019-06-04 01:12:27 -0400

    Category Detail TabLayout

--- app/src/main/java/codemaestro/co/punchclock/Fragments/CategoryDetailFragment.java
+++ app/src/main/java/codemaestro/co/punchclock/Fragments/CategoryDetailFragment.java
@@ -4,9 +4,10 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,15 +15,11 @@ import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import codemaestro.co.punchclock.Adapters.GoalsSmallAdapter;
-import codemaestro.co.punchclock.Adapters.HabitsSmallAdapter;
 import codemaestro.co.punchclock.Model.Category;
-import codemaestro.co.punchclock.Model.Goal;
-import codemaestro.co.punchclock.Model.Habit;
 import codemaestro.co.punchclock.R;
 import codemaestro.co.punchclock.ViewModel.CategoryViewModel;
 import codemaestro.co.punchclock.ViewModel.GoalViewModel;
@@ -37,12 +34,22 @@ public class CategoryDetailFragment extends Fragment {
     private static final String TAG = "CategoryDetailFragment";
     private NavController navController;
 
+    private ViewPager categoryDetailViewPager;
+    private TabLayout tabLayout;
+    View view;
+    DetailAdapter adapter;
+
     public CategoryDetailFragment() {
         // Required empty public constructor
     }
 
     public static CategoryDetailFragment newInstance() {
         return new CategoryDetailFragment();
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+
+
     }
 
     @Override
@@ -52,57 +59,96 @@ public class CategoryDetailFragment extends Fragment {
         GoalViewModel goalViewModel = ViewModelProviders.of(this).get(GoalViewModel.class);
         HabitViewModel habitViewModel = ViewModelProviders.of(this).get(HabitViewModel.class);
         categoryTitle = view.findViewById(R.id.categoryTitle);
-        categoryDescription = view.findViewById(R.id.categoryDescription);
-        goalOneView = view.findViewById(R.id.categoryDetailGoalOne);
-        goalTwoView = view.findViewById(R.id.categoryDetailGoalTwo);
-        habitOneView = view.findViewById(R.id.categoryDetailHabitOne);
-        habitTwoView = view.findViewById(R.id.categoryDetailHabitTwo);
+
+        categoryDetailViewPager = view.findViewById(R.id.categoryDetailViewPager);
+        tabLayout = view.findViewById(R.id.categoryDetailTabLayout);
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+
+
+        //Here we have to pass ChildFragmentManager instead of FragmentManager.
+        adapter = new DetailAdapter(getChildFragmentManager());
+        adapter.addFragment(new CategoryDetailGoalTab(), "Goals");
+        adapter.addFragment(new CategoryDetailHabitTab(), "Habits");
+        adapter.addFragment(new CategoryDetailJournalTab(), "Journal");
+        categoryDetailViewPager.setAdapter(adapter);
+
+        tabLayout.setupWithViewPager(categoryDetailViewPager);
+
+
         String money = getArguments().getString("category_title");
-
-
-
         categoryViewModel.getCurrentCategory(money).observe(this, new Observer<Category>() {
             @Override
             public void onChanged(@Nullable Category category) {
                 categoryTitle.setText(category.getCategoryName());
             }
         });
-
-        goalViewModel.getAllCategoryGoals(money).observe(this, new Observer<List<Goal>>() {
-            @Override
-            public void onChanged(@Nullable List<Goal> goals) {
-                if (goals != null) {
-                    Goal goalOne = goals.get(0);
-                    goalOneView.setText(goalOne.getGoalName());
-                    goalTwoView.setText(goals.get(1).getGoalName());
-//                goalOneView.setText(""+goals.get(3));
-                }
-            }
-        });
-
-        //Todo: Habit
-        habitViewModel.getHabitsByCategoryName(money).observe(this, new Observer<List<Habit>>() {
-            @Override
-            public void onChanged(@Nullable List<Habit> habits) {
-                if (habits != null) {
-                    habitOneView.setText(habits.get(0).getHabitName());
-                    habitTwoView.setText(habits.get(1).getHabitName());
-                }
-            }
-        });
-
-
-
         return view;
+    }
+
+    static class DetailAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> fragments = new ArrayList<>();
+        private final List<String> fragmentTitles = new ArrayList<>();
+
+        DetailAdapter(android.support.v4.app.FragmentManager fm) {
+            super(fm);
+        }
+
+        void addFragment(Fragment fragment, String title) {
+            fragments.add(fragment);
+            fragmentTitles.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitles.get(position);
+        }
     }
 
 
 }
 
 
-
 // To go in onCreateView
+
+//categoryDescription = view.findViewById(R.id.categoryDescription);
+//        goalOneView = view.findViewById(R.id.categoryDetailGoalOne);
+//        goalTwoView = view.findViewById(R.id.categoryDetailGoalTwo);
+//        habitOneView = view.findViewById(R.id.categoryDetailHabitOne);
+//        habitTwoView = view.findViewById(R.id.categoryDetailHabitTwo);
+//goalViewModel.getAllCategoryGoals(money).observe(this, new Observer<List<Goal>>() {
+//            @Override
+//            public void onChanged(@Nullable List<Goal> goals) {
+//                if (goals != null) {
+//                    Goal goalOne = goals.get(0);
+//                    goalOneView.setText(goalOne.getGoalName());
+//                    goalTwoView.setText(goals.get(1).getGoalName());
+////                goalOneView.setText(""+goals.get(3));
+//                }
+//            }
+//        });
 //
+//        habitViewModel.getHabitsByCategoryName(money).observe(this, new Observer<List<Habit>>() {
+//            @Override
+//            public void onChanged(@Nullable List<Habit> habits) {
+//                if (habits != null) {
+//                    habitOneView.setText(habits.get(0).getHabitName());
+//                    habitTwoView.setText(habits.get(1).getHabitName());
+//                }
+//            }
+//        });
+
+//
+// To go in onCreateView
 //        final GoalsSmallAdapter goalsAdpater = new GoalsSmallAdapter(getActivity());
 
 //        final RecyclerView goalsRecyclerView = view.findViewById(R.id.goalsRecyclerView);
---
+++ app/src/main/java/codemaestro/co/punchclock/Fragments/CategoryDetailJournalTab.java
@@ -0,0 +1,19 @@
+package codemaestro.co.punchclock.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import codemaestro.co.punchclock.R;
+
+public class CategoryDetailJournalTab extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tab_journal, container, false);
+
+        return view;
+    }
+}
